f9bf80577826b8999092fdff502e4abe645c3588
carsonmagnuson <carsonmagnuson@gmail.com>
2025-06-25 05:11:00 -0400

    pull OG id getter to its own module, implement select_orthologs and fix tests

--- backend-refactor/pipe_fun/ortholog_functions.py
+++ backend-refactor/pipe_fun/ortholog_functions.py
@@ -2,19 +2,45 @@ from Bio.SeqRecord import SeqRecord
 from Bio import SeqIO
 from typing import List
 
-import requests, io
+import requests, io, os
 
-def compile_orthologs(
+
+def select_ortholog_group(
     ncbi_gene_id: str,
     taxanomic_level_id: str,
-    ) -> List[SeqRecord]:
-
+    ) -> str:
     """
-    Compiles a list of orthologs to the specified gene at the specified taxanomic level.
+    Determines an appropriate ortholog group given a specified gene and taxonomic level.
 
     Args:
-        ncbi_gene_id: What is the id of the ID of the gene you want to compile orthologs for?
-        taxanomic_level_id: At what taxanomic level do you want to look?
+        ncbi_gene_id: What is the id of the ID of the gene you want to find orthologs for?
+        taxanomic_level_id: At what taxanomic level do you want to focus?
+
+    Returns:
+        An OrthoDB ortholog group ID
+
+    """
+
+    orthoDB_API = "https://data.orthodb.org/v12/" 
+
+    # STEP 1: Query OrthoDB for an ortholog group ID and take the first available one.
+    ortholog_group_id = requests.get(f"{orthoDB_API}search", params={
+        'ncbi': ncbi_gene_id,
+        'level': taxanomic_level_id,
+        }).json()['data'][0]
+
+    #STEP 2: Return the ID
+    return ortholog_group_id
+
+def compile_orthologs(
+        ortholog_group_id: str
+        ) -> List[SeqRecord]:
+
+    """
+    Compiles a list of orthologs from a specified ortholog group
+
+    Args:
+        ncbi_gene_id: What is the ID of the ortholog group you want to compile a list from?
 
     Returns:
         A list of seqRecord objects, with seq, id, name and description -> id is orthoDB_id:orthoDB_OG_id I think, name is the actual name
@@ -23,26 +49,68 @@ def compile_orthologs(
 
     orthoDB_API = "https://data.orthodb.org/v12/" 
 
-    # STEP 1: Query OrthoDB for an ortholog group ID and take the first available one
-    ortholog_group_id = requests.get(f"{orthoDB_API}search", params={
-        'ncbi': ncbi_gene_id,
-        'level': taxanomic_level_id,
-        }).json()['data'][0]
 
-    # STEP 2: Query OrthoDB for ortholog group fasta data
+    # STEP 1: Query OrthoDB for ortholog group fasta data.
     ortholog_fasta_compilation = requests.get(f"{orthoDB_API}fasta", params={
         'id': ortholog_group_id,
         'seqtype': 'cds',
         }).text
     
-    # STEP 3: convert fasta text to list of seqRecords and return it (setting the name to the actual name instead of the id again with some magic)
+    # STEP 2: convert fasta text to list of seqRecords and return it (setting the name to the actual name instead of the id again with some magic).
     ortholog_compilation = [SeqRecord(record.seq, id=record.id, name=record.description.split('organism_name":"')[1].split('"')[0], description=record.description) for record in SeqIO.parse(io.StringIO(ortholog_fasta_compilation), "fasta")]
     return ortholog_compilation
-    
+
+
+def select_orthologs(
+        ortholog_compilation: List[SeqRecord],
+        k_sequences: int,
+        ortholog_group_id: str,
+        preselected_sequences: list = [],
+        ) -> str:
+    """
+    Selects orthologs from a compiled seqRecord list for positive selection analysis.
+
+    Args:
+        ortholog_compilation: What list of orthologs do you want to look through?
+        k_sequences: How many orthologs do you want, total, for your analysis (preselected sequences are included)?
+        ortholog_group_id: What is the ID of the ortholog group you're selecting from?
+        preselected_sequences: Are there any orthologs (or at least the original gene to which all the others are orthologous) you want to preselect?
+
+    Returns:
+        A filepath to an outputted fasta file.
+
+    """
+
+    # STEP 1: Remove the preselected sequences from the ortholog compilation.
+    for record in preselected_sequences:
+        ortholog_compilation.remove(record)
+
+    # STEP 2: Place ortholog compilation in order of most suitable to least suitable.
+
+
+    # STEP 3: Extend preselected list with most suitable sequence candidates
+    remaining_k = k_sequences - len(preselected_sequences)
+    selected_orthologs = preselected_sequences + ortholog_compilation[0:remaining_k]
+
+    # STEP 4: Check for/create a directory for output using the ortholog group id
+    output_directory = ortholog_group_id + "_run"
+    if not os.path.exists(output_directory):
+        os.makedirs(output_directory)
+
+    # STEP 5: Write selected list to a fasta file and return the path. Using length of the selected list in the name.
+    output_filepath = f"{output_directory}/{len(selected_orthologs)}_selected_orthologs.fasta"
+    SeqIO.write(selected_orthologs, output_filepath, "fasta")
+    return output_filepath
+ 
+
 if __name__ == '__main__':
 
     test_ncbi_ids = ["173042", "173402", "3039"] # C. elegans spe-39, C. elegans lin-39, Human HBA1
     test_levels = ["6231", "6231", "40674"]    # Nematoda, Nematoda, Mammalia
+    
+    ortholog_group_id = select_ortholog_group(test_ncbi_ids[0], test_levels[0])
+    ortholog_compilation = compile_orthologs(ortholog_group_id)
+    selected_orthologs = select_orthologs(ortholog_compilation, 10, ortholog_group_id)
 
-    print(compile_orthologs(test_ncbi_ids[0], test_levels[0])[0])
+
 
--- backend-refactor/test_pipe_fun.py
+++ backend-refactor/test_pipe_fun.py
@@ -1,10 +1,19 @@
 from pipe_fun import ortholog_functions
 
-
 test_ncbi_ids = ["173042", "173402", "3039"] # C. elegans spe-39, C. elegans lin-39, Human HBA1
 test_levels = ["6231", "6231", "40674"]    # Nematoda, Nematoda, Mammalia
+test_ortholog_group_ids = ["29865at6231"]
+
+def test_select_ortholog_group():
+    assert ortholog_functions.select_ortholog_group(test_ncbi_ids[0], test_levels[0]) == test_ortholog_group_ids[0]
 
 def test_compile_orthologs():
-    ortholog_compilation = ortholog_functions.compile_orthologs(test_ncbi_ids[0], test_levels[0])
+    ortholog_compilation = ortholog_functions.compile_orthologs(test_ortholog_group_ids[0])
     print(len(ortholog_compilation))
     assert len(ortholog_compilation) > 10
+
+def test_select_orthologs(k = 8):
+    selected_orthologs = ortholog_functions.select_orthologs(ortholog_functions.compile_orthologs(test_ortholog_group_ids[0]), k, test_ortholog_group_ids[0])
+    print(selected_orthologs)
+    assert int(selected_orthologs.split('/')[1][0]) == k # Check that the file has the correct number of selected orthologs
+
