5e582170840700078f899e26745d175526fd6531
Carson Magnuson <80717302+carsonmagnuson@users.noreply.github.com>
2025-12-18 07:10:49 -0500

    Initialize argparse for command line parsing
    Add argparse import and define parse function

--- pipeline/parser.py
+++ pipeline/parser.py
@@ -1,1 +1,7 @@
+import argparse
 
+def parse(input: str) -> argparse.Namespace:
+  """
+  """
+  parser = argparse.ArgumentParser(description="A not so simple command line tool for running an optimized positive selection analysis on a gene of interest.")
+  
