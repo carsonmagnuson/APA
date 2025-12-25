bce8db4ef9df59186f4e2c2c1cca01605c48c961
carsonmagnuson <carsonmagnuson@gmail.com>
2025-12-24 21:12:29 -0500

    add docstring to parser

--- pipeline/parser.py
+++ pipeline/parser.py
@@ -1,7 +1,15 @@
 import argparse
 
-def parse(input: str) -> argparse.Namespace:
+def parse(inputted: str) -> argparse.Namespace:
   """
+    Parses input to determine which pipeline to run and how to run it.
+
+    Args:
+        inputted: what is the string of flags that followed the call?
+
+    Returns:
+        Should return a list of arguments? I'm not sure how this works yet.
+
   """
   parser = argparse.ArgumentParser(description="A not so simple command line tool for running an optimized positive selection analysis on a gene of interest.")
   
